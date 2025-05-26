565495a02bbc6709c5d7c455519591b5acc3bfc6
PokerMonster <50902632+PokerMonster@users.noreply.github.com>
2025-05-26 16:29:11 +0800

    app.py
    # 還原換行符號：把 "\\n" 換成 "\n"

--- app.py
+++ app.py
@@ -5,7 +5,14 @@ from firebase_admin import credentials, initialize_app
 load_dotenv()
 
 # 讀取並解析 JSON
-cred_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
-cred_dict = json.loads(cred_json)
+raw = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+
+# 先轉成字典
+cred_dict = json.loads(raw)
+
+# 還原換行符號：把 "\\n" 換成 "\n"
+cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
+
+# 用 firebase_admin 初始化
 cred = credentials.Certificate(cred_dict)
 initialize_app(cred)
