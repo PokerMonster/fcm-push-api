0fb7f1430c4fdaf92bcfd8799f7bab35a2895b6e
PokerMonster <50902632+PokerMonster@users.noreply.github.com>
2025-05-26 16:32:22 +0800

    app.py
    gunicorn app:app

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from dotenv import load_dotenv
+from flask import Flask
 import os, json
+from dotenv import load_dotenv
 from firebase_admin import credentials, initialize_app
 
 load_dotenv()
 
-# 讀取並解析 JSON
-raw = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
-
-# 先轉成字典
-cred_dict = json.loads(raw)
-
-# 還原換行符號：把 "\\n" 換成 "\n"
+cred_json = os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+cred_dict = json.loads(cred_json)
 cred_dict["private_key"] = cred_dict["private_key"].replace("\\n", "\n")
-
-# 用 firebase_admin 初始化
 cred = credentials.Certificate(cred_dict)
 initialize_app(cred)
+
+app = Flask(__name__)  # ← 必須是 app！
+
+@app.route("/")
+def index():
+    return "✅ Flask + Firebase Ready on Render!"
