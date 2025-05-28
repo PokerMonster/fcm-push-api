229a08b09171aa074f6e8a3eaab7159cedea5bde
PokerMonster <50902632+PokerMonster@users.noreply.github.com>
2025-05-28 11:54:38 +0800

    app.py
    Update  mysql port

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from firebase_admin import credentials, initialize_app
 import mysql.connector
 
 db = mysql.connector.connect(
-    host=os.environ.get("fcm-mysql-api.in2soft.net:51306"),
+    host=os.environ.get("fcm-mysql-api.in2soft.net"),
+    port=os.environ.get("51306"),
     user=os.environ.get("fcmuser"),
     password=os.environ.get("FcMepAss9021"),
     database=os.environ.get("fcmdb")
