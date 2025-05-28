10f0693674f7cfdbfdb22a056c6b43588cf36c1c
PokerMonster <50902632+PokerMonster@users.noreply.github.com>
2025-05-28 18:17:34 +0800

    multicast_message.py
    Update

--- routes/multicast_message.py
+++ routes/multicast_message.py
@@ -1,38 +1,21 @@
 # routes/multicast_message.py
 from flask import Blueprint
 import os, json
-import firebase_admin
-from firebase_admin import credentials, messaging
+#import firebase_admin
+from firebase_admin import messaging
 from dotenv import load_dotenv
 
 multicast_bp = Blueprint('multicast_bp', __name__)
 
-load_dotenv()
-if not firebase_admin._apps:
-    cred = credentials.Certificate(cred_dict)
-    firebase_admin.initialize_app(cred)
-
 def send_multicast_notification(tokens, title, body, data=None):
-    """
-    發送推播通知給多個設備。
-
-    :param tokens: List[str] - FCM 註冊 token 列表
-    :param title: str - 通知標題
-    :param body: str - 通知內容
-    :param data: dict - 附加資料（可選）
-    :return: dict - 包含成功和失敗的統計資訊
-    """
     message = messaging.MulticastMessage(
         notification=messaging.Notification(title=title, body=body),
         data=data or {},
         tokens=tokens,
     )
 
-    #response = messaging.send_multicast(message)
-
     try:
         response = messaging.send_multicast(message)
-    # 处理响应
     except Exception as e:
         print(f"发送通知时发生错误: {e}")
         return {
@@ -41,6 +24,7 @@ def send_multicast_notification(tokens, title, body, data=None):
             "responses": [],
             "error": str(e)
         }
+
     if not tokens:
         print("设备令牌列表为空，无法发送通知。")
         return {
@@ -49,6 +33,7 @@ def send_multicast_notification(tokens, title, body, data=None):
             "responses": [],
             "error": "No device tokens provided."
         }
+
     for idx, resp in enumerate(response.responses):
         if resp.success:
             print(f"訊息成功發送至設備 {tokens[idx]}")
