8b5c8a9ddbc5e4d7f52de29c64b9736a1b4f505d
PokerMonster <50902632+PokerMonster@users.noreply.github.com>
2025-05-28 17:06:16 +0800

    multicast_message.py
    multicast_bp

--- routes/multicast_message.py
+++ routes/multicast_message.py
@@ -4,6 +4,8 @@ import os
 import firebase_admin
 from firebase_admin import credentials, messaging
 
+multicast_bp = Blueprint('multicast_bp', __name__)
+
 # 初始化 Firebase Admin SDK
 if not firebase_admin._apps:
     cred = credentials.Certificate(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS"))
